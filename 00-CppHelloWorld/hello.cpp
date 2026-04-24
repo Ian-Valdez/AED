e00e7fc4561624e5824ec4f8247beb1c52d8979c
Ian Valdez <ivaldez@frba.utn.edu.ar>
2026-04-24 17:24:15 -0300

    Agrego .gitignore, muevo config a la raíz y ajusto formato de entrega

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -1,3 +1,8 @@
+/* TP #00: Hello, World!
+ * Demostración de capacidad para editar, compilar y ejecutar programas C++.
+ * Alumno: Ian Valdez
+ * Última modificación: 24/04/2026
+ */
 #include <iostream>
 int main(){
     std::cout << "Hello, World! :)\n";

59d7673d57653c1a21299e824de1d6f08eabf1d8
Ian Valdez <ivaldez@frba.utn.edu.ar>
2026-04-25 20:15:11 -0300

    Ajusto output y gitignore según consigna del aula virtual

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -1,9 +1,13 @@
 /* TP #00: Hello, World!
  * Demostración de capacidad para editar, compilar y ejecutar programas C++.
  * Alumno: Ian Valdez
- * Última modificación: 24/04/2026
+ * Última modificación: 25/04/2026
  */
 #include <iostream>
-int main(){
-    std::cout << "Hello, World! :)\n";
+
+int main() {
+    std::cout << "Hola, mundo! :)\n"
+                 "Nombre y apellido: Ian Valdez\n"
+                 "Curso de AED: K1053\n"
+                 "Grupo asignado: 8\n";
 }
